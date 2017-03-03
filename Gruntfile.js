08316de5080aac44e9f7419e6100558507019a29
Robert <rbryan21@gmail.com>
2017-03-03 09:39:45 -0500

    Final prep for presentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,56 +5,56 @@
 */
 
 // Node.js convention to make something accesible like a "public" method
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-      concat : {   // create a task named 'concat'
-          options : {  // specify options 
-              separator : '\n\n//----------------------------------------------------------\n\n',
-          },
-          dist : {    // create a job for the task
-            src: ['site/js/*.js'],  // grab any js file within the js directory
-            dest : 'site/concat/script.js'  // concatenate them within the script.js
-          },
-      },
-      sass : {
-          options : {
-              // What do you what the CSS to look like within the file?
-              outputStyle : 'compressed'
-          },
-          iCanBeNamedAnything : {
-              files : [{
-                  src : 'sass/style.scss',
-                  dest : 'site/css/style.css'
-              }]
-          }
-      },
-      watch : {
-          options : {
-              spawn : false,
-              livereload : true
-          },
-          scripts : {
-              // What files do we want to watch?
-              files : ['sass/*.scss', 'site/js/*.js', 'site/index.html'],
-              // Run these tasks again when the above files are modified
-              tasks : ['concat', 'sass'] 
-          }
-      }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        concat: {   // create a task named 'concat'
+            options: {  // specify options 
+                separator: '\n\n//----------------------------------------------------------\n\n',
+            },
+            dist: {    // create a job for the task
+                src: ['developmentJS/*.js'],  // grab any js file within the js directory
+                dest: 'site/js/script.js'  // concatenate them within the script.js
+            },
+        },
+        sass: {
+            options: {
+                // What do you what the CSS to look like within the file?
+                outputStyle: 'compressed'
+            },
+            iCanBeNamedAnything: {
+                files: [{
+                    src: 'developmentSass/style.scss',
+                    dest: 'site/css/style.css'
+                }]
+            }
+        },
+        watch: {
+            options: {
+                spawn: false
+            },
+            scripts: {
+                // What files do we want to watch?
+                files: ['developmentSass/*.scss', 'developmentJS/*.js', 'site/index.html'],
+                // Run these tasks again when the above files are modified
+                tasks: ['concat', 'sass']
+            }
+        }
+    });
 
-  // Load the plugin that provide the "concat", "sass", and "watch" tasks. 
-  //                    &
-  // npm install grunt-contrib-concat --save-dev
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // Load the plugin that provide the "concat", "sass", and "watch" tasks. 
+    //                    &
+    // npm install grunt-contrib-concat --save-dev
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s)
-  //    - Automatically run when you enter 'grunt' in command line
-  // Otherwise run a task with grunt 'taskName'
-  grunt.registerTask('default', ['concat', 'sass', 'watch']);
+    // Default task(s)
+    //    - Automatically run when you enter 'grunt' in command line
+    // Otherwise run a task with grunt 'taskName'
+    grunt.registerTask('default', ['concat', 'sass', 'watch']);
 };
+
 
 
